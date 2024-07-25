3ccb7513e7d3ba8d19e62aba38ad930cf5187120
Pedro <pedroh.fokus@gmail.com>
2024-07-24 22:38:14 -0300

    View Login

--- public/index.php
+++ public/index.php
@@ -1,12 +1,5 @@
 <?php
 
 require_once(dirname(__FILE__,2) . '/src/config/config.php');
-require_once(dirname(__FILE__,2) . '/src/models/User.php');
-
-$user = new User(['name' => 'Pedro', 'email' => 'pedro@gmail.com']);
-
-echo User::getSelect(['id' => 1], 'name','email');
-echo '<br>';
-echo User::getSelect(['name' => 'Chaves','email' => 'chaves@cod3r.com.br']);
-
+require_once(dirname(__FILE__,2) . '/src/view/login.php');
 
